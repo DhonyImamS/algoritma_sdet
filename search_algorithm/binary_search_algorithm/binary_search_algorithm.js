f564eaf68395c8cedf0cc1c1d370bd7dfd8775d2
Dhony Imam Saputra <dhony.imam.saputra@gmail.com>
2024-10-03 15:44:18 +0700

    Update binary_search_algorithm.js

--- search_algorithm/binary_search_algorithm/binary_search_algorithm.js
+++ search_algorithm/binary_search_algorithm/binary_search_algorithm.js
@@ -2,34 +2,37 @@
     Given Sorted Array [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 */
 
-function binarySearch(arrayInput, searchValue) {
-    let startReference = 0, medianReference = 0, endReference = arrayInput.length - 1;
-    let isSearching = true;
-    let foundingIndex = 'Not Found';;
-
-    while(isSearching) {
-        [startReference, medianReference, endReference] = calculateBoundaries(startReference, endReference);
-        console.log(startReference, "|", medianReference, "|", endReference);
-
-        if (searchValue > arrayInput[endReference]) {
-            isSearching = false;
-        } else {
-            if (searchValue > arrayInput[medianReference]) {
-                startReference = medianReference + 1;
-            } else if ( searchValue < arrayInput[medianReference]) {
-                endReference = medianReference - 1;
-            } else {
-                if (searchValue === arrayInput[medianReference]) foundingIndex = `Found at index - ${medianReference}`;
-                isSearching = false;
-            }
-        }
+function binarySearch(sortedNumsArr, searchNumber) {
+    let isSearchFinish = false;
+    let startIdx = 0;
+    let endIdx = sortedNumsArr.length - 1;
+    
+    function getMedianIdx(startIdx, endIdx) {
+        return Math.floor((startIdx + endIdx) / 2);
     }
-    return console.log(foundingIndex);
+    
+    while (!isSearchFinish) {
+        const medianIndex = getMedianIdx(startIdx, endIdx);
+        
+        if (searchNumber === sortedNumsArr[medianIndex]) {
+            console.log(`Number already Found`);
+            isSearchFinish = true;
+        } else if ( searchNumber > sortedNumsArr[medianIndex]) {
+            if (searchNumber <= sortedNumsArr[medianIndex + 1]) {
+                startIdx = medianIndex;    
+            } else {
+                console.log(`Number not Found`);
+                isSearchFinish = true;
+            }
+        } else if ( searchNumber < sortedNumsArr[medianIndex]) {
+            if (searchNumber >= sortedNumsArr[medianIndex - 1]) {
+                endIdx = medianIndex;
+            } else {
+                console.log(`Number not Found`);
+                isSearchFinish = true;
+            }
+        } 
+    }
 }
 
-function calculateBoundaries(startIndex, endingIndex) {
-    const medianValue = Math.floor((startIndex + endingIndex) / 2);
-    return [startIndex, medianValue, endingIndex];
-}
-
-binarySearch([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4);
+binarySearch([1, 2, 3, 4, 5, 6, 8, 10], 0)
